4a7daed6f065ac310501fa811950d91bbead7e05
Peter Tran <peter@adlabs.live>
2020-03-18 19:43:09 -0400

    Added objects from API

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,13 @@
 class WelcomeController < ApplicationController
   def test
-    @response = HTTParty.get("http://api.openweathermap.org/data/2.5/weather?zip=27615,us&appid=#{ENV["open_weather_map_key"]}")
+    response = HTTParty.get("http://api.openweathermap.org/data/2.5/weather?zip=27615,us&units=imperial&appid=#{ENV["open_weather_map_key"]}")
 
+    @location = response["name"]
+    @temp = response["main"]["temp"]
+    @cloudiness = response["clouds"]["all"]
+    @weather_icon = response["weather"][0]["icon"]
+    @weather_description = response["weather"][0]["description"]
+    @windiness = response["wind"]["speed"]
     
   end
 
