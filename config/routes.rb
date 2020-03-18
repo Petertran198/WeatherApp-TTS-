bea3eae37e3bee5962d09ed15192fdaf896318a5
Peter Tran <peter@adlabs.live>
2020-03-18 18:35:38 -0400

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'test', to: 'welcome#test'
+  get 'index', to: 'welcome#index'
+  root 'welcome#test'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
