1bc7b96738f16ae218a28b40b5bbbecbbb5f8f39
Peter Tran <peter@adlabs.live>
2020-03-18 19:11:16 -0400

    Added api key

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,8 @@
 class WelcomeController < ApplicationController
   def test
+    @response = HTTParty.get("http://api.openweathermap.org/data/2.5/weather?zip=27615,us&appid=#{ENV["open_weather_map_key"]}")
+
+    
   end
 
   def index
