9845e34f100f99c3738de40451a26e2266195d50
Peter Tran <peter@adlabs.live>
2020-03-18 20:19:01 -0400

    Added test and index action

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -12,5 +12,22 @@ class WelcomeController < ApplicationController
   end
 
   def index
+    if params[:zip] != nil && params[:zip] != ""
+    response = HTTParty.get("http://api.openweathermap.org/data/2.5/weather?zip=#{params[:zip]},us&units=imperial&appid=#{ENV["open_weather_map_key"]}")
+      @status = response["cod"]
+      if @status != '404'
+          @location = response["name"]
+          @id = response["weather"][0]["id"]
+          @temp = response["main"]["temp"]
+          @cloudiness = response["clouds"]["all"]
+          @weather_icon = response["weather"][0]["icon"]
+          @weather_description = response["weather"][0]["description"]
+          @windiness = response["wind"]["speed"]
+      else
+        @error = response["message"]
+      end
+    end
   end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'test', to: 'welcome#test'
   get 'index', to: 'welcome#index'
+  post 'index', to: 'welcome#index'
   root 'welcome#test'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
